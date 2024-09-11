82ce262aabd4cbd9d7ef675a2d80445d7530c334
Morgan Polak <polakmaor@gmail.com>
2024-09-11 20:34:24 +0100

    REF: refactors the logic for playing bands into multiple functions

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -20,11 +20,13 @@ import { Card } from '../models/card.model';
 import { Op } from 'sequelize';
 import { Region } from '../models/region.model';
 import PlayerRegion from '../models/player_region.model';
+import { Color } from '../types/game.interface';
+import { IHandleTribeOptions, IRemainingCardsOptions } from '../types/command.interface';
 
 const {
     CENTAUR,
     DRAGON,
-    DWARF,
+    // DWARF,
     ELF,
     GIANT,
     HALFING,
@@ -82,32 +84,75 @@ class CommandService {
             payload: updatedGameState
         });
     }
-
     static async handlePlayBand(game: Game, player: Player, payload: IPlayBandPayload): Promise<INextActionPayload> {
         let nextAction;
 
-        const bandCards = player.cards.filter(card => payload.cardIds.includes(card.id));
+        const bandCards = this.getBandCards(player, payload.cardIds);
+        const leader = this.getLeader(bandCards, payload.leaderId);
+        const band = this.getBandDetails(leader, bandCards, payload);
+        let remainingCards = this.getRemainingCards(player, payload.cardIds);
 
-        let remainingCards = player.cards.filter(card => !payload.cardIds.includes(card.id));
+        this.validateTribe(band.tribe);
 
-        const leader = bandCards.find(card => card.id === payload.leaderId);
-        const tribe = leader.tribe.name;
+        await this.assignCardsToBand(player, payload, bandCards);
+        const region = await this.getRegion(game, band.color);
+        const playerRegion = await this.getPlayerRegion(region, player);
+
+        nextAction = await this.handleTribeLogic({
+            band,
+            game,
+            player,
+            payload,
+            remainingCards,
+            playerRegion
+        });
+
+        await this.handleRemainingCards({
+            remainingCards,
+            nextAction,
+            player,
+            payload,
+            tribe: band.tribe
+        });
+
+        return nextAction;
+    }
+
+    static getBandCards(player: Player, cardIds: number[]): Card[] {
+        return player.cards.filter(card => cardIds.includes(card.id));
+    }
+
+    static getRemainingCards(player: Player, cardIds: number[]): Card[] {
+        return player.cards.filter(card => !cardIds.includes(card.id));
+    }
+
+    static getLeader(bandCards: Card[], leaderId: number): Card {
+        return bandCards.find(card => card.id === leaderId);
+    }
+
+    static getBandDetails(leader: Card, bandCards: Card[], payload: IPlayBandPayload) {
+        let tribe = leader.tribe.name;
         let color = leader.color;
-
         let bandSize = bandCards.length;
 
         if (tribe === MINOTAUR) {
             bandSize++;
         }
 
-        if (tribe === SKELETON) {
-            throw new CustomException(ERROR_BAD_REQUEST, 'A Skeleton cannot be the leader of a band');
-        }
-
         if (tribe === WINGFOLK) {
             color = payload.regionColor;
         }
 
+        return { tribe, color, bandSize };
+    }
+
+    static validateTribe(tribe: TribeName) {
+        if (tribe === SKELETON) {
+            throw new CustomException(ERROR_BAD_REQUEST, 'A Skeleton cannot be the leader of a band');
+        }
+    }
+
+    static async assignCardsToBand(player: Player, payload: IPlayBandPayload, bandCards: Card[]) {
         await Card.update({
             state: CardState.IN_BAND,
             playerId: player.id,
@@ -115,118 +160,150 @@ class CommandService {
             index: null,
         }, {
             where: {
-                id: {
-                    [Op.in]: payload.cardIds,
-                }
+                id: { [Op.in]: bandCards.map(card => card.id) }
             }
         });
+    }
 
-        const region = await Region.findOne({
-            where: {
-                gameId: game.id,
-                color,
-            }
-        });
+    static async getRegion(game: Game, color: Color) {
+        return Region.findOne({ where: { gameId: game.id, color } });
+    }
 
-        const playerRegion = await PlayerRegion.findOne({
+    static async getPlayerRegion(region: Region, player: Player) {
+        return PlayerRegion.findOne({
             where: {
                 regionId: region.id,
                 playerId: player.id
             }
         });
+    }
 
-        if (bandSize > playerRegion.tokens) {
-            await playerRegion.update({
-                tokens: playerRegion.tokens + 1,
-            });
+    static async handleTribeLogic({
+        band,
+        game,
+        player,
+        remainingCards,
+        playerRegion,
+    }: IHandleTribeOptions): Promise<INextActionPayload> {
+        let nextAction;
 
-            if (tribe  === CENTAUR && remainingCards.length) {
-                nextAction = {
-                    type: ActionType.PLAY_BAND
-                };
+        const {
+            bandSize,
+            color,
+            tribe,
+        } = band;
+
+        if (tribe !== HALFING && bandSize > playerRegion.tokens) {
+            await playerRegion.update({ tokens: playerRegion.tokens + 1 });
+
+            if (tribe === CENTAUR && remainingCards.length) {
+                nextAction = { type: ActionType.PLAY_BAND };
             }
         }
 
-        if (tribe === ORC && !player.orcTokens.includes(color)) {
-            await player.update({
-                orcTokens: [...player.orcTokens, color]
-            });
+        switch (tribe) {
+            case ORC:
+                await this.handleOrcTokens(player, color);
+                break;
+            case GIANT:
+                await this.handleGiantBand(game, player, bandSize);
+                break;
+            case MERFOLK:
+                await this.handleMerfolkTrack(player, bandSize);
+                break;
+            case WIZARD:
+                await this.handleWizardDraw(game, player, bandSize);
+                break;
+            case TROLL:
+                await this.handleTrollTokens(game, player, bandSize);
+                break;
         }
 
-        if (tribe === GIANT) {
-            const largestGiantBand = await Player.findOne({
-                where: {
-                    gameId: game.id,
-                    giantTokenValue: {
-                        [Op.gte]: bandSize,
-                    }
+        return nextAction;
+    }
+
+    static async handleOrcTokens(player: Player, color: Color) {
+        if (!player.orcTokens.includes(color)) {
+            await player.update({ orcTokens: [...player.orcTokens, color] });
+        }
+    }
+
+    static async handleGiantBand(game: Game, player: Player, bandSize: number) {
+        const largestGiantBand = await Player.findOne({
+            where: {
+                gameId: game.id,
+                giantTokenValue: {
+                    [Op.gte]: bandSize
                 }
-            });
-
-            if (!largestGiantBand) {
-                await player.update({
-                    giantTokenValue: bandSize,
-                    points: player.points + 2,
-                });
             }
-        }
+        });
 
-        if (tribe === MERFOLK) {
-            let freeTokens = 0;
-
-            const merfolkTrackCheckpoints =  [
-                3, 7, 12, 18
-            ];
-
-            for (let i = 1; i <= bandSize; i++) {
-                if (merfolkTrackCheckpoints.includes(player.merfolkTrackScore + i)) {
-                    freeTokens++
-                    // TODO: add 'FREE_TOKEN' next action for each free token
-                }
-            }
-
+        if (!largestGiantBand) {
             await player.update({
-                merfolkTrackScore: player.merfolkTrackScore + bandSize,
+                giantTokenValue: bandSize,
+                points: player.points + 2,
             });
         }
+    }
 
-        if (tribe === WIZARD) {
-            const cardsInMarket = game.cards.filter(card => card.state === CardState.IN_MARKET);
-            const maxDrawSize = bandSize > cardsInMarket.length ? cardsInMarket.length : bandSize;
-            for (let i = 0; i < maxDrawSize; i++) {
-                await CommandService.handleDrawCard(game, player);
+    static async handleMerfolkTrack(player: Player, bandSize: number) {
+        const merfolkTrackCheckpoints = [3, 7, 12, 18];
+        let freeTokens = 0;
+
+        for (let i = 1; i <= bandSize; i++) {
+            if (merfolkTrackCheckpoints.includes(player.merfolkTrackScore + i)) {
+                freeTokens++;
+                // TODO: add 'FREE_TOKEN' next action for each free token
             }
         }
 
-        if (tribe === TROLL) {
+        await player.update({
+            merfolkTrackScore: player.merfolkTrackScore + bandSize,
+        });
+    }
 
-            let claimedTokens: number[] = [];
+    static async handleWizardDraw(game: Game, player: Player, bandSize: number) {
+        const cardsInMarket = game.cards.filter(card => card.state === CardState.IN_MARKET);
+        const maxDrawSize = Math.min(bandSize, cardsInMarket.length);
 
-            game.players.map(player => {
-                claimedTokens = [...claimedTokens, ...player.trollTokens];
-            });
+        for (let i = 0; i < maxDrawSize; i++) {
+            await CommandService.handleDrawCard(game, player);
+        }
+    }
 
-            let trollTokens = [6,5,4,3,2,1].filter((token) =>
-                !claimedTokens.includes(token)
-            );
+    static async handleTrollTokens(game: Game, player: Player, bandSize: number) {
+        let claimedTokens: number[] = [];
 
-            if (trollTokens.includes(bandSize)) {
-                await player.update({
-                    trollTokens: [...player.trollTokens, bandSize]
-                });
-            } else {
-               const smallerToken = trollTokens.find(token => token < bandSize);
+        game.players.map(player => {
+            claimedTokens = [...claimedTokens, ...player.trollTokens]
+        });
 
-               if (smallerToken) {
-                    await player.update({
-                        trollTokens: [...player.trollTokens, smallerToken]
-                    });
-               }
+        const trollTokens = [6, 5, 4, 3, 2, 1].filter(token => !claimedTokens.includes(token));
+
+        if (trollTokens.includes(bandSize)) {
+            await player.update({ trollTokens: [...player.trollTokens, bandSize] });
+        } else {
+            const smallerToken = trollTokens.find(token => token < bandSize);
+
+            if (smallerToken) {
+                await player.update({ trollTokens: [...player.trollTokens, smallerToken] });
             }
         }
+    }
 
+    static filterElfCards(remainingCards: Card[], payload: IPlayBandPayload) {
+        return remainingCards.filter(card => !payload.cardIdsToKeep.includes(card.id));
+    }
+
+    static async handleRemainingCards({
+        remainingCards,
+        nextAction,
+        player,
+        payload,
+        tribe,
+    }: IRemainingCardsOptions) {
         if (tribe === ELF) {
-            remainingCards = remainingCards.filter(card => !payload.cardIdsToKeep.includes(card.id));
+            remainingCards = this.filterElfCards(remainingCards, payload);
         }
 
         if (remainingCards.length && !nextAction) {
@@ -238,14 +315,10 @@ class CommandService {
             }, {
                 where: {
                     playerId: player.id,
-                    id: {
-                        [Op.in]: remainingCards.map(card => card.id)
-                    }
+                    id: { [Op.in]: remainingCards.map(card => card.id) }
                 }
             });
         }
-
-        return nextAction;
     }
 
     static async handlePickUpCard(game: Game, activePlayer: Player, payload: IActionPayload): Promise<void> {
---
+++ src/types/command.interface.ts
@@ -0,0 +1,30 @@
+import { Card } from '../models/card.model';
+import { Game } from '../models/game.model';
+import { Player } from '../models/player.model';
+import PlayerRegion from '../models/player_region.model';
+import { INextActionPayload, IPlayBandPayload } from './action.interface';
+import { Color } from './game.interface';
+import { TribeName } from './tribe.interface';
+
+export interface IBandDetails {
+    tribe: string;
+    bandSize: number;
+    color: Color;
+}
+
+export interface IHandleTribeOptions {
+    band: IBandDetails;
+    game: Game;
+    player: Player;
+    payload: IPlayBandPayload;
+    remainingCards: Card[];
+    playerRegion: PlayerRegion;
+}
+
+export interface IRemainingCardsOptions {
+    remainingCards: Card[];
+    nextAction: INextActionPayload;
+    player: Player;
+    payload: IPlayBandPayload;
+    tribe: TribeName;
+}
