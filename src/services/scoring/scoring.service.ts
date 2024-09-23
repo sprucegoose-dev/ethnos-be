87fdcbde53488417dec6cc79290243a8afbf45ab
Morgan Polak <polakmaor@gmail.com>
2024-09-23 19:06:33 +0100

    ENH: working on scoring regions and other end of game scoring

--- src/services/scoring/scoring.service.ts
+++ src/services/scoring/scoring.service.ts
@@ -7,6 +7,7 @@ import { CardState, IGroupedCards } from '@interfaces/card.interface';
 import { TribeName } from '@interfaces/tribe.interface';
 import Region from '../../models/region.model';
 import Game from '../../models/game.model';
+import PlayerRegion from '../../models/player_region.model';
 
 const BAND_VALUES: { [key: number]: number } = {
     1: 0,
@@ -67,7 +68,7 @@ export default class ScoringService {
             }
         });
 
-        const playerRegions = await Region.findAll({
+        const playerRegions = await PlayerRegion.findAll({
             where: {
                 regionId: {
                     [Op.in]: regions.map(region => region.id)
@@ -75,17 +76,76 @@ export default class ScoringService {
             }
         });
 
-
-        for (const region of regions) {
-
-            // depending on age and who has the most tokens, give out scores
-
-        }
-
         const finalAge = game.players.length >= 4 ? 3 : 2;
 
-        if (game.age === finalAge) {
-            await this.scoreOrcBoards(game.players);
+        const trollTokenTotals: { [playerId: number]: number } = {};
+
+        const totalPoints: { [playerId: number]: number } = {};
+
+        for (const player of game.players) {
+            let trollTokenSum = 0;
+
+            for (const trollToken of player.trollTokens) {
+                trollTokenSum += trollToken;
+            }
+
+            trollTokenTotals[player.id] = trollTokenSum;
+
+            totalPoints[player.id] = player.points;
+        }
+
+        const regionRankings: { [rank: string]: number[] } = {};
+
+        for (const region of regions) {
+            playerRegions
+                .filter(playerRegion => playerRegion.regionId === region.id)
+                .sort((a, b) => b.tokens - a.tokens)
+                .map(playerRegion => {
+                    const rankKey = `${playerRegion.tokens}.${trollTokenTotals[playerRegion.playerId]}`;
+
+                    if (regionRankings[rankKey]) {
+                        regionRankings[rankKey].push(playerRegion.playerId);
+                    } else {
+                        regionRankings[rankKey] = [playerRegion.playerId];
+                    }
+                });
+
+            const regionPoints = region.values.slice(0, game.age).sort((a, b) => b - a);
+
+            for (const playerIds of Object.values(regionRankings)) {
+                if (!regionPoints.length) {
+                    continue;
+                }
+
+                const totalValue = regionPoints.splice(0, playerIds.length).reduce<number>((acc, currentValue) => acc += currentValue, 0);
+
+                const pointsPerPlayer = Math.floor(totalValue / playerIds.length);
+
+                for (const playerId of playerIds) {
+                    totalPoints[playerId] += pointsPerPlayer;
+                }
+            }
+        }
+
+        for (const player of game.players) {
+            // get player who is highest on the Merfolk track (in case of tie, trolls break ties)
+
+            // get player with the largest giant band (in case of tie, trolls break ties)
+
+            if (game.age === finalAge) {
+                totalPoints[player.id] += this.scoreOrcBoard(player);
+            }
+        }
+
+
+        for (const [playerId, points] of Object.entries(totalPoints)) {
+            await Player.update({
+                points
+            }, {
+                where: {
+                    id: playerId
+                }
+            })
         }
     }
 
@@ -97,8 +157,18 @@ export default class ScoringService {
 
     }
 
-    static async scoreOrcBoards(players: Player[]) {
+    static scoreOrcBoard(player: Player): number {
+        const orcBoardPoints: {[tokens: number]: number} = {
+            0: 0,
+            1: 1,
+            2: 3,
+            3: 6,
+            4: 10,
+            5: 15,
+            6: 20,
+        };
 
+        return orcBoardPoints[player.orcTokens.length];
     }
 
 }
