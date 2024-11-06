76464d3e77ecc0604ec05a5c23a0451c255bd7e1
Morgan Polak <polakmaor@gmail.com>
2024-11-06 21:20:49 +0000

    TST: adds tests for the BotService (canAddTokenToRegion)

--- src/services/bot/bot.service.test.ts
+++ src/services/bot/bot.service.test.ts
@@ -11,6 +11,8 @@ import NextAction from '../../models/nextAction.model';
 import { ActionType } from '../../interfaces/action.interface';
 import PlayerRegion from '../../models/player_region.model';
 import { Op } from 'sequelize';
+import ScoringService from '../scoring/scoring.service';
+import { TribeName } from '../../interfaces/tribe.interface';
 
 describe('BotService', () => {
     describe('addTokenToRegion', () => {
@@ -143,4 +145,81 @@ describe('BotService', () => {
         });
     });
 
+    describe('canAddTokenToRegion', () => {
+        // let gameId: number;
+        let playerA: Player;
+        let regions: Region[];
+
+        beforeEach(async () => {
+            const result = await createGame();
+
+            // gameId = result.gameId;
+            playerA = result.playerA;
+            regions = result.gameState.regions;
+        });
+
+        afterEach(async () => await Game.truncate());
+
+        it("should return true if a player's band is large enough to add a token to a region", async () => {
+            await PlayerRegion.create({
+                playerId: playerA.id,
+                regionId: regions[0].id,
+                tokens: 2
+            });
+
+            const updatedRegion = await Region.findOne({
+                where: {
+                    id: regions[0].id
+                },
+                include: [
+                    {
+                        model: PlayerRegion,
+                        as: 'playerTokens'
+                    }
+                ]
+            });
+
+            const bandDetails = {
+                color: regions[0].color,
+                bandSize: 3,
+                tribe: TribeName.TROLLS,
+                points: ScoringService.getBandPoints(3),
+            }
+
+            const canAddToken = BotService.canAddTokenToRegion(updatedRegion, bandDetails, playerA);
+
+            expect(canAddToken).toBe(true);
+        });
+
+        it("should return false if a player's band is NOT large enough to add a token to a region", async () => {
+            await PlayerRegion.create({
+                playerId: playerA.id,
+                regionId: regions[0].id,
+                tokens: 2
+            });
+
+            const updatedRegion = await Region.findOne({
+                where: {
+                    id: regions[0].id
+                },
+                include: [
+                    {
+                        model: PlayerRegion,
+                        as: 'playerTokens'
+                    }
+                ]
+            });
+
+            const bandDetails = {
+                color: regions[0].color,
+                bandSize: 2,
+                tribe: TribeName.TROLLS,
+                points: ScoringService.getBandPoints(3),
+            }
+
+            const canAddToken = BotService.canAddTokenToRegion(updatedRegion, bandDetails, playerA);
+
+            expect(canAddToken).toBe(false);
+        });
+    });
 });
