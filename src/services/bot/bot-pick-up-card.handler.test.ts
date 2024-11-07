3c4226531f11a6fc1cd5311fd4fb56d165ca902f
Morgan Polak <polakmaor@gmail.com>
2024-11-07 18:47:29 +0000

    TST: adds tests for the BotPickUpCardHandler (emptyHandPickUpOrDrawCard)

--- src/services/bot/bot-pick-up-card.handler.test.ts
+++ src/services/bot/bot-pick-up-card.handler.test.ts
@@ -6,12 +6,14 @@ import { CardState } from '@interfaces/card.interface';
 import { IGameState } from '@interfaces/game.interface';
 
 import ActionService from '@services/action/action.service';
+import PlayerService from '@services/player/player.service';
 
 import {
     createGame,
+    returnPlayerCardsToDeck,
 } from '../test-helpers';
 import BotPickUpCardHandler from './bot-pick-up-card.handler';
-import PlayerService from '../player/player.service';
+import GameService from '../game/game.service';
 
 describe('BotPickUpCardHandler', () => {
 
@@ -32,7 +34,7 @@ describe('BotPickUpCardHandler', () => {
 
         afterEach(async () => await Game.truncate());
 
-        it("should return false if the player has cards in their hand", async () => {
+        it('should return false if the player has cards in their hand', async () => {
             const player = await PlayerService.getPlayerWithCards(playerA.id);
             const cardsInHand = player.cards.filter(card => card.state === CardState.IN_HAND);
             const actions = await ActionService.getActions(gameId, player.userId);
@@ -40,5 +42,23 @@ describe('BotPickUpCardHandler', () => {
             expect(response).toBe(false);
         });
 
+        it("should pick up a random card from the market if a player's hand is empty and there are cards in the market", async () => {
+            await returnPlayerCardsToDeck(playerA.id);
+            let cardsInHand: Card[] = [];
+            const actions = await ActionService.getActions(gameId, playerA.userId);
+
+            expect(cardsInMarket.length).toBe(8);
+
+            const response = await BotPickUpCardHandler.emptyHandPickUpOrDrawCard(actions, cardsInHand, cardsInMarket, playerA);
+            const updatedPlayer = await PlayerService.getPlayerWithCards(playerA.id);
+            const updatedGame = await GameService.getState(gameId);
+
+            cardsInHand = updatedPlayer.cards.filter(card => card.state === CardState.IN_HAND);
+            cardsInMarket = updatedGame.cards.filter(card => card.state === CardState.IN_MARKET);
+
+            expect(response).toBe(true);
+            expect(cardsInHand.length).toBe(1);
+            expect(cardsInMarket.length).toBe(7);
+        });
     });
 });
