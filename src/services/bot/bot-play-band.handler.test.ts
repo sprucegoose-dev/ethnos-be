6221e04e99a3eb3c82d90ca83d024a2a692248a7
Morgan Polak <polakmaor@gmail.com>
2024-11-09 09:24:22 +0000

    TST: adds tests for the BotPlayBandHandler (getPlayerTokensInRegion)

---
+++ src/services/bot/bot-play-band.handler.test.ts
@@ -0,0 +1,56 @@
+import Player from '@models/player.model';
+import PlayerRegion from '@models/player_region.model';
+import Region from '@models/region.model';
+import Game from '@models/game.model';
+
+import {
+    IGameState
+} from '@interfaces/game.interface';
+
+import {
+    createGame,
+} from '../test-helpers';
+import BotPlayBandHandler from './bot-play-band.handler';
+
+
+describe('BotPlayBandHandler', () => {
+
+    describe('getPlayerTokensInRegion', () => {
+        let playerA: Player;
+        let gameState: IGameState;
+
+        beforeEach(async () => {
+            const result = await createGame();
+            playerA = result.playerA;
+            gameState = result.gameState;
+        });
+
+        afterEach(async () => await Game.truncate());
+
+        it('should return 0 if a player has no tokens in a region', () => {
+            const result = BotPlayBandHandler.getPlayerTokensInRegion(gameState.regions[0], playerA)
+            expect(result).toBe(0);
+        });
+
+        it("should return the count of a player's token in a region if a player has tokens in that region", async () => {
+            await PlayerRegion.create({
+                playerId: playerA.id,
+                regionId: gameState.regions[0].id,
+                tokens: 3
+            });
+
+            const region = await Region.findOne({
+                where: {
+                    id: gameState.regions[0].id,
+                },
+                include: {
+                    model: PlayerRegion,
+                    as: 'playerTokens'
+                }
+            })
+
+            const result = BotPlayBandHandler.getPlayerTokensInRegion(region, playerA)
+            expect(result).toBe(3);
+        });
+    });
+});
